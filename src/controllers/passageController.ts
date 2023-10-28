34b831889466efb6988b9f749d6e8ccc2d89d68a
viana <1211134@isep.ipp.pt>
2023-10-28 11:12:31 +0100

    US250 #11 - Implementation

--- src/controllers/passageController.ts
+++ src/controllers/passageController.ts
@@ -6,6 +6,8 @@ import { Result } from "../core/logic/Result";
 import IPassageController from './IControllers/IPassageController';
 import IPassageService from '../services/IServices/IPassageService';
 import IPassageDTO from '../dto/IPassageDTO';
+import { STATUS_CODES } from 'http';
+import { floor } from 'lodash';
 
 
 @Service()
@@ -42,6 +44,22 @@ export default class PassageController implements IPassageController {
             const passageListDTO = passagesListOrError.getValue();
             return res.json( passageListDTO ).status(201);
 
+        } catch (e) {
+            return next(e);
+        }
+    }
+
+    public async updatePassage(req: Request, res: Response, next: NextFunction) {
+        try {
+            const passageOrError = await this.passageServiceInstance.updatePassage(req.body as IPassageDTO) as Result<IPassageDTO>;
+            
+            if (passageOrError.isFailure) {
+                return res.status(402).send(passageOrError.errorValue());
+            }
+
+            const passageDTO = passageOrError.getValue();
+            
+            return res.json(passageDTO).status(201);
         } catch (e) {
             return next(e);
         }
--- src/services/IServices/IPassageService.ts
+++ src/services/IServices/IPassageService.ts
@@ -5,5 +5,6 @@ export default interface IPassageService {
 
     createPassage(passageDTO: IPassageDTO): Promise<Result<IPassageDTO>>;
     getAllPassages(): Promise<Result<IPassageDTO[]>>;
+    updatePassage(passageDTO: IPassageDTO): Promise<Result<IPassageDTO>>;
 
 }
