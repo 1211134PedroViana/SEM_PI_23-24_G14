a97f66b2a0ee41efebf85aee5892b059d23f12a4
Pedro Ferreira <1210825@isep.ipp.pt>
2023-11-03 14:46:40 +0000

    Added a Navbar

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CampusSideBarComponent } from '../campus-side-bar/campus-side-bar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CampusSideBarComponent
+    CampusSideBarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
