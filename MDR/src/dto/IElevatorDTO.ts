6c45fdc5d4145e3a0913cb0fb35fe2fce050bab7
pedrocosta <1211439@isep.ipp.pt>
2023-11-10 20:16:43 +0000

    Fix some code.

--- MDR/src/dto/IElevatorDTO.ts
+++ MDR/src/dto/IElevatorDTO.ts
@@ -1,15 +1,15 @@
 export default interface IElevatorDTO {
-    id: string;
-    code: string;
-    location: {
-        positionX: number;
-        positionY: number;
-        direction: string;
-    }
-    buildingId: string;
-    floorList: string[];
-    brand: string;
-    model: string;
-    serialNumber: string;
-    description: string;
+  id: string;
+  code: string;
+  location: {
+    positionX: number;
+    positionY: number;
+    direction: string;
+  };
+  buildingId: string;
+  floorList: string[];
+  brand: string;
+  model: string;
+  serialNumber: string;
+  description: string;
 }
--- MDR/src/dto/IPassageDTO.ts
+++ MDR/src/dto/IPassageDTO.ts
@@ -1,10 +1,10 @@
 export default interface IPassageDTO {
-    id: string;
-    fromFloorId: string;
-    toFloorId: string;
-    location: {
-        positionX: number;
-        positionY: number;
-        direction: string;
-    }
+  id: string;
+  fromFloorId: string;
+  toFloorId: string;
+  location: {
+    positionX: number;
+    positionY: number;
+    direction: string;
+  };
 }
