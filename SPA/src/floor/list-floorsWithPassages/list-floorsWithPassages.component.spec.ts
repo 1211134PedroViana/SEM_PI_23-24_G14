456c3729481de45404babc4cd4da42681a246709
Pedro Viana <1211134@isep.ipp.pt>
2023-11-15 10:22:59 +0000

    #31 US1200 Start Development

---
+++ SPA/src/floor/list-floorsWithPassages/list-floorsWithPassages.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListFloorsWithPassagesComponent } from './list-floorsWithPassages.componet';
+
+describe('ListFloorsWithPassagesComponent', () => {
+  let component: ListFloorsWithPassagesComponent;
+  let fixture: ComponentFixture<ListFloorsWithPassagesComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListFloorsWithPassagesComponent]
+    });
+    fixture = TestBed.createComponent(ListFloorsWithPassagesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
