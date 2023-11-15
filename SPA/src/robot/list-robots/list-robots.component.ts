5876ea45f71aab07a74b0ea37bce57f3f4f56f88
Davidpinho243 <1211436@isep.ipp.pt>
2023-11-15 10:46:55 +0000

    #22 US390 Code development.

---
+++ SPA/src/robot/list-robots/list-robots.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { RobotService } from 'src/robotService/robot.service';
+import Robot from 'src/robotService/robot';
+
+@Component({
+    selector: 'app-list-robots',
+    templateUrl: './list-robots.component.html',
+    styleUrls: ['./list-robots.component.css']
+})
+
+export class ListRobotsComponent implements OnInit {
+
+    robots: Robot[] = [];
+
+    selectedRobot: any;
+
+    constructor(private robotService: RobotService) { }
+
+    ngOnInit(): void {
+        this.loadRobots();
+    }
+
+    loadRobots() {
+        this.robotService.getAllRobots()
+        .pipe(
+            tap((response) => {
+                this.robots = response;
+                console.log('Robots listed successfully', response);
+            }),
+            catchError((error) => {
+                console.error('Error occurred while listing the robots', error);
+                throw error;
+            })
+        )
+        .subscribe()
+    }
+}
