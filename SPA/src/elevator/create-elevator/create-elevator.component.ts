9f47fe0fcafd28997325b6fe36e9e28131fd4c66
pedrocosta <1211439@isep.ipp.pt>
2023-11-16 10:22:33 +0000

    #44 Start US 1130.

---
+++ SPA/src/elevator/create-elevator/create-elevator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-elevator',
+  templateUrl: './create-elevator.component.html',
+  styleUrls: ['./create-elevator.component.css']
+})
+export class CreateElevatorComponent {
+
+}
