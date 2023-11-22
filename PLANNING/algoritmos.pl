af7b4ac03658bd4dffd1d77d5e1e6f3c71067aa9
Pedro Ferreira <1210825@isep.ipp.pt>
2023-11-22 23:29:23 +0000

    #64 algoritmos

--- PLANNING/algoritmos.pl
+++ PLANNING/algoritmos.pl
@@ -2,6 +2,8 @@
 :-consult('bc_floorMaps').
 :-consult('bc_coordenadas').
 
+:-dynamic ligacel/2.
+
 
 % Algoritmo - Caminho entre edificios
 caminho_edificios(EdOr,EdDest,LEdCam):-
@@ -31,12 +33,12 @@ segue_pisos(PisoDest1,PisoDest,[EdDest],[elev(PisoDest1,PisoDest)]):-
     elevador(EdDest,LPisos), member(PisoDest1,LPisos), 
     member(PisoDest,LPisos).
 
-segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[cor(PisoAct,PisoSeg)|LOutrasLig]):-
+segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[pass(PisoAct,PisoSeg)|LOutrasLig]):-
     (corredor(EdAct,EdSeg,PisoAct,PisoSeg);
     corredor(EdSeg,EdAct,PisoSeg,PisoAct)),
     segue_pisos(PisoSeg,PisoDest,[EdSeg|LOutrosEd],LOutrasLig).
 
-segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[elev(PisoAct,PisoAct1),cor(PisoAct1,PisoSeg)|LOutrasLig]):-
+segue_pisos(PisoAct,PisoDest,[EdAct,EdSeg|LOutrosEd],[elev(PisoAct,PisoAct1),pass(PisoAct1,PisoSeg)|LOutrasLig]):-
     (corredor(EdAct,EdSeg,PisoAct1,PisoSeg);
     corredor(EdSeg,EdAct,PisoSeg,PisoAct1)), PisoAct1\==PisoAct,
     elevador(EdAct,LPisos), member(PisoAct,LPisos), member(PisoAct1,LPisos),
@@ -49,25 +51,24 @@ melhor_caminho_pisos(PisoOr,PisoDest,LLigMelhor):-
     findall(LLig,caminho_pisos(PisoOr,PisoDest,_,LLig),LLLig),
     menos_elevadores(LLLig,LLigMelhor,_,_).
 
-menos_elevadores([LLig],LLig,NElev,NCor):-
+menos_elevadores([LLig],LLig,NElev,NCor):- 
     conta(LLig,NElev,NCor).
-      
+
 menos_elevadores([LLig|OutrosLLig],LLigR,NElevR,NCorR):-
-    menos_elevadores(OutrosLLig,LLigM,NElev,NCor),
-    conta(LLig,NElev1,NCor1),
-    (((NElev1<NElev;(NElev1==NElev,NCor1<NCor)),!,
-    NElevR is NElev1, NCorR is NCor1,LLigR=LLig);
-    (NElevR is NElev,NCorR is NCor,LLigR=LLigM)).
+   menos_elevadores(OutrosLLig,LLigM,NElev,NCor),
+   conta(LLig,NElev1,NCor1),
+   (((NElev1<NElev;(NElev1==NElev,NCor1<NCor)),!,
+   NElevR is NElev1, NCorR is NCor1,LLigR=LLig);
+   (NElevR is NElev,NCorR is NCor,LLigR=LLigM)).
 
 conta([],0,0).
 
 conta([elev(_,_)|L],NElev,NCor):-
-    conta(L,NElevL,NCor),
-    NElev is NElevL+1.
+   conta(L,NElevL,NCor),NElev is NElevL+1.
 
 conta([pass(_,_)|L],NElev,NCor):-
-    conta(L,NElev,NCorL),
-    NCor is NCorL+1.
+   conta(L,NElev,NCorL), NCor is NCorL+1.
+
 
 % Predicado para extrair o piso de um elemento do caminho
 extrair_piso_origem(elev(Origem, _), Piso) :-
@@ -88,7 +89,7 @@ cria_grafo(_,_,0):-!.
 
 cria_grafo(Piso,Col,Lin):-
    cria_grafo_lin(Piso,Col,Lin),
-   Lin1 is Lin1,
+   Lin1 is Lin-1,
    cria_grafo(Piso,Col,Lin1).
 
 cria_grafo_lin(_,0,_):-!.
@@ -98,6 +99,7 @@ cria_grafo_lin(Piso,Col,Lin):-
    ColS is Col+1, ColA is Col-1, 
    LinS is Lin+1,LinA is Lin-1,
    ((m(Piso,ColS,Lin,0),assertz(ligacel(Piso,cel(Col,Lin),cel(ColS,Lin)));true)),
+   write('Ligacel: '),write(ligacel(Piso,cel(Col,Lin),cel(ColS,Lin))),nl,
    ((m(Piso,ColA,Lin,0),assertz(ligacel(Piso,cel(Col,Lin),cel(ColA,Lin)));true)),
    ((m(Piso,Col,LinS,0),assertz(ligacel(Piso,cel(Col,Lin),cel(Col,LinS)));true)),
    ((m(Piso,Col,LinA,0),assertz(ligacel(Piso,cel(Col,Lin),cel(Col,LinA)));true)),
@@ -129,22 +131,47 @@ find_caminho(PisoOr, PisoDest):-
 processar_caminho([_]).     
 
 processar_caminho([Elemento1, Elemento2|Resto]):-
-    processar_pisos_elemento(Elemento1, Elemento2),    % processa os pisos do elemento atual
+    processar_pares_elemento(Elemento1, Elemento2),    % processa os pisos do elemento atual
     processar_caminho([Elemento2 | Resto]).
 
 % processa os pisos de um elemento do caminho
-processar_pisos_elemento(elev(Origem, Destino), pass(Origem, Destino)) :-
-    extrair_piso_destino(elev(Origem, Destino), Piso),
-    %extrair_piso_destino(pass(Origem, Destino), PisoPassDest),
-    %coordenadas(Piso, ElevPosX, ElevPosY),
-    %coordenadas(Piso, PisoPassDest, PassPosX, PassPosY, _, _),
-    dimensoes(Piso, Col, Lin),
-    cria_grafo(Piso, Col, Lin).       % chama o cria_grafo/3 para o piso de origem
+processar_pares_elemento(elev(_, ElevDestino), pass(PassOrigem, PassDestino)) :-
+    coordenadas(ElevDestino, ElevPosX, ElevPosY),
+    coordenadas(ElevDestino, PassDestino, PassPosX, PassPosY, _, _),
+    dimensoes(ElevDestino, Col, Lin),
+    cria_grafo(ElevDestino, Col, Lin).       % chama o cria_grafo/3 para o piso de origem
 
-processar_pisos_elemento(pass(Origem, Destino), elev(Origem, Destino)) :-
-    %extrair_piso_origem(pass(Origem, Destino), Piso),
-    extrair_piso_destino(pass(Origem, Destino), PisoPassDest),
-    %coordenadas(Piso, PisoPassDest, _, _, PassPosX, PassPosY),
-    %coordenadas(PisoPassDest, ElevPosX, ElevPosY),
-    dimensoes(PisoPassDest, Col, Lin),
-    cria_grafo(PisoPassDest, Col, Lin).
+processar_pares_elemento(pass(PassOrigem, PassDestino), elev(ElevOrigem, _)) :-
+    coordenadas(PassOrigem, PassDestino, _, _, PassPosX, PassPosY),
+    coordenadas(ElevOrigem, ElevPosX, ElevPosY),
+    dimensoes(PassDestino, Col, Lin),
+    cria_grafo(PassDestino, Col, Lin).
+
+processar_pares_elemento(pass(PassOrigem, PassDestino), pass(Pass2Origem, Pass2Destino)) :-
+    coordenadas(PassOrigem, PassDestino, _, _, PassPosX, PassPosY),
+    coordenadas(Pass2Origem, Pass2Destino, Pass2PosX, Pass2PosY, _,_),
+    dimensoes(PassDestino, Col, Lin),
+    cria_grafo(PassDestino, Col, Lin).
+
+% Algoritmo DFS
+dfs(Piso,Orig,Dest,Cam):-
+   dfs2(Piso,Orig,Dest,[Orig],Cam).
+
+dfs2(_,Dest,Dest,LA,Cam):-
+   reverse(LA,Cam).
+
+dfs2(Piso,Act,Dest,LA,Cam):-
+   ligacel(Piso,Act,X), \+ member(X,LA),
+   dfs2(Piso,X,Dest,[X|LA],Cam).
+
+all_dfs(Piso,Orig,Dest,LCam):-
+   findall(Cam,dfs(Piso,Orig,Dest,Cam),LCam).
+
+better_dfs(Piso,Orig,Dest,Cam):-
+   all_dfs(Piso,Orig,Dest,LCam), shortlist(LCam,Cam,_), write('List:'), write(Cam).
+
+shortlist([L],L,N):-!,length(L,N).
+
+shortlist([L|LL],Lm,Nm):-
+   shortlist(LL,Lm1,Nm1), length(L,NL),
+   ((NL<Nm1,!,Lm=L,Nm is NL); (Lm=Lm1,Nm is Nm1)).
