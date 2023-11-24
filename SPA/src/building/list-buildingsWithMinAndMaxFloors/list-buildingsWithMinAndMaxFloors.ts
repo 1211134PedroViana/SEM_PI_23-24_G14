b1f6b3ba73be0fe07770d487db14747edcd0f0ff
Davidpinho243 <1211436@isep.ipp.pt>
2023-11-24 23:06:02 +0000

    #27 US1080 Development.

--- SPA/src/building/list-buildingsWithMinAndMaxFloors/list-buildingsWithMinAndMaxFloors.ts
+++ SPA/src/building/list-buildingsWithMinAndMaxFloors/list-buildingsWithMinAndMaxFloors.ts
@@ -4,45 +4,51 @@ import { catchError, tap } from 'rxjs/operators';
 import Building from 'src/buildingService/building';
 
 @Component({
-    selector: 'app-list-buildingsWithMinAndMaxFloors',
-    templateUrl: './list-buildingsWithMinAndMaxFloors.component.html',
-    styleUrls: ['./list-buildingsWithMinAndMaxFloors.component.css']
+  selector: 'app-list-buildingsWithMinAndMaxFloors',
+  templateUrl: './list-buildingsWithMinAndMaxFloors.component.html',
+  styleUrls: ['./list-buildingsWithMinAndMaxFloors.component.css']
 })
-
 export class ListBuildingsWithMinAndMaxFloorsComponent implements OnInit {
 
-    buildings: Building[] = [];
+  buildings: Building[] = [];
+  selectedBuilding: any;
+  min: any;
+  max: any;
+  formSubmitted = false; //Control
 
-    selectedBuilding: any;
+  constructor(private buildingService: BuildingService) { }
 
-    min: any;
-    max: any;
+  ngOnInit(): void {
 
-    constructor(private buildingService: BuildingService) { }
+  }
 
-    ngOnInit(): void {
-        this.loadBuildings();
+  loadBuildings() {
+    this.buildingService.getAllBuildingsWithMinAndMaxFloors(this.min, this.max)
+      .pipe(
+        tap((response) => {
+          this.buildings = response;
+          console.log('Buildings listed successfully', response);
+        }),
+        catchError((error) => {
+          console.error('Error occurred while listing the buildings', error);
+          throw error;
+        })
+      )
+      .subscribe();
+  }
+
+  isFormOpen = false;
+  openForm(building: Building) {
+    this.buildingService.openForm(building);
+    this.isFormOpen = true;
+  }
+
+  submitForm() {
+    if (this.min !== undefined && this.max !== undefined) {
+      this.loadBuildings();
+      this.formSubmitted = true; 
+    } else {
+      console.warn('Por favor, insira valores mínimos e máximos.');
     }
-
-    loadBuildings() {
-        this.buildingService.getAllBuildingsWithMinAndMaxFloors(this.min, this.max)
-            .pipe(
-                tap((response) => {
-                    this.buildings = response;
-                    console.log('Buildings listed successfully', response);
-                }),
-                catchError((error) => {
-                    console.error('Error occurred while listing the buildings', error);
-                    throw error;
-                })
-            )
-            .subscribe()
-    }
-
-    isFormOpen = false;
-    openForm(building: Building) {
-      // Pass building data to the form component (e.g., using a service)
-      this.buildingService.openForm(building);
-      this.isFormOpen = true;
-    }
+  }
 }
