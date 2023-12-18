c1028dc9945d52e097f80b30942ba5a0db3ba675
pedrocosta <1211439@isep.ipp.pt>
2023-12-18 10:56:54 +0000

    #92 Development US.

---
+++ SPA/src/terms-and-conditions/terms-and-conditions.component.cy.ts
@@ -0,0 +1,7 @@
+import { TermsAndConditionsComponent } from './terms-and-conditions.component'
+
+describe('TermsAndConditionsComponent', () => {
+  it('should mount', () => {
+    cy.mount(TermsAndConditionsComponent)
+  })
+})
