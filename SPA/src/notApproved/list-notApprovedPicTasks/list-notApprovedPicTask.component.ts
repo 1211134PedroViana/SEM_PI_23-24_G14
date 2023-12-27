a06b77efae42507a85ca3dfd287f4805bb85662e
Pedro Viana <1211134@isep.ipp.pt>
2023-12-27 18:48:03 +0000

    #86 Bug fixing

--- SPA/src/notApproved/list-notApprovedPicTasks/list-notApprovedPicTask.component.ts
+++ SPA/src/notApproved/list-notApprovedPicTasks/list-notApprovedPicTask.component.ts
@@ -3,18 +3,20 @@ import { catchError, tap } from 'rxjs/operators';
 import SurveillanceTask from "../../taskService/surveillanceTask";
 import {TaskService} from "../../taskService/task.service";
 import PickupAndDeliveryTask from "../../taskService/pickupAndDeliveryTask";
+import { SystemUserService } from 'src/systemUserService/systemUser.service';
 
 
 @Component({
-  selector: 'app-list-elevators',
+  selector: 'app-list-notApprovedPicTask',
   templateUrl: './list-notApprovedPicTask.component.html',
   styleUrls: ['./list-notApprovedPicTasks.component.css']
 })
 export class ListNotApprovedPicTaskComponent implements OnInit {
 
   tasks: PickupAndDeliveryTask[] = [];
+  parsedTasks: PickupAndDeliveryTask[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService, private userService: SystemUserService) { }
 
   ngOnInit(): void {
     this.loadAllTasks();
@@ -31,6 +33,7 @@ export class ListNotApprovedPicTaskComponent implements OnInit {
         tap((response) => {
           if (status === "Pending") {
             this.tasks = response;
+            this.parsePickList();
           } else if (status === "Reject") {
             this.tasks = response;
           }
@@ -42,6 +45,41 @@ export class ListNotApprovedPicTaskComponent implements OnInit {
         })
       )
       .subscribe();
+  }
+
+  parsePickList() {
+    for(let i = 0; i < this.tasks.length; i++) {
+      let user: any;
+
+      this.userService.getUserById(this.tasks[i].userId)
+      .pipe(
+        tap((response) => {
+          user = response;
+
+          const pickupAndDeliveryTask = ({
+            pickupPlace: this.tasks[i].pickupPlace,
+            deliveryPlace: this.tasks[i].deliveryPlace,
+            pickupPersonName: this.tasks[i].pickupPersonName,
+            pickupPersonPhoneNumber: this.tasks[i].pickupPersonPhoneNumber,
+            deliveryPersonName: this.tasks[i].deliveryPersonName,
+            deliveryPersonPhoneNumber: this.tasks[i].deliveryPersonPhoneNumber,
+            description: this.tasks[i].description,
+            confirmationCode: this.tasks[i].confirmationCode,
+            status: this.tasks[i].status,
+            userId: user.email
+          }) as PickupAndDeliveryTask;
+
+          this.parsedTasks.push(pickupAndDeliveryTask);
+
+        }),
+        catchError((error) => {
+          console.error('Error occurred while getting the User', error);
+          throw error;
+        })
+      )
+      .subscribe()  
+
+    }
   }
 
     isFormOpen = false;
--- SPA/src/notApproved/list-notApprovedSurTasks/list-notApprovedSurTask.component.ts
+++ SPA/src/notApproved/list-notApprovedSurTasks/list-notApprovedSurTask.component.ts
@@ -2,18 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { catchError, tap } from 'rxjs/operators';
 import SurveillanceTask from "../../taskService/surveillanceTask";
 import {TaskService} from "../../taskService/task.service";
+import { FloorService } from 'src/floorService/floor-service';
+import { Router } from '@angular/router';
+import { SystemUserService } from 'src/systemUserService/systemUser.service';
+import { BuildingService } from 'src/buildingService/building.service';
 
 
 @Component({
-  selector: 'app-list-elevators',
+  selector: 'app-list-notApprovedSurTask',
   templateUrl: './list-notApprovedSurTask.component.html',
   styleUrls: ['./list-notApprovedSurTasks.component.css']
 })
 export class ListNotApprovedSurTaskComponent implements OnInit {
 
   tasks: SurveillanceTask[] = [];
+  parsedSurvTasks: SurveillanceTask[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService, private userService: SystemUserService, private buildingService: BuildingService,
+    private floorService: FloorService, private router: Router) { }
 
   ngOnInit(): void {
     this.loadAllTasks();
@@ -30,6 +36,7 @@ export class ListNotApprovedSurTaskComponent implements OnInit {
         tap((response) => {
           if (status === "Pending") {
             this.tasks = response;
+            this.parseSurvList();
           } else if (status === "Reject") {
             this.tasks = response;
           }
@@ -43,6 +50,64 @@ export class ListNotApprovedSurTaskComponent implements OnInit {
       .subscribe();
   }
 
+  parseSurvList() {
+    for(let i = 0; i < this.tasks.length; i++) {
+      let building: any;
+      let user: any;
+      let floor: any;
+
+      this.userService.getUserById(this.tasks[i].userId)
+      .pipe(
+        tap((response) => {
+          user = response;
+
+          this.buildingService.getBuildingById(this.tasks[i].buildingId)
+          .pipe(
+            tap((response) => {
+              building = response;
+
+              this.floorService.getFloorById(this.tasks[i].floorId)
+                .pipe(
+                tap((response) => {
+                  floor =  response;
+
+                  const surveillanceTask = {
+                    buildingId: building.name,
+                    floorId: floor.floorNumber,
+                    startPlace: this.tasks[i].startPlace,
+                    endPlace: this.tasks[i].endPlace,
+                    phoneNumber: this.tasks[i].phoneNumber,
+                    status: this.tasks[i].status,
+                    userId: user.email
+                  } as SurveillanceTask;
+                
+                  this.parsedSurvTasks.push(surveillanceTask);
+                }),
+                catchError((error) => {
+                  console.error('Error occurred while getting the Floor', error);
+                  throw error;
+                })
+                )
+                .subscribe()
+
+            }),
+          catchError((error) => {
+          console.error('Error occurred while getting the Building', error);
+          throw error;
+        })
+        )
+        .subscribe()
+
+        }),
+        catchError((error) => {
+          console.error('Error occurred while getting the User', error);
+          throw error;
+        })
+      )
+      .subscribe()  
+
+    }
+  }
     isFormOpen = false;
 
   openForm(tasks: SurveillanceTask) {
